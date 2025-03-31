f7e71d3cc28f7df0bf6b86164066332b4f703835
Aditya Gambhir <67105262+Aditya-gam@users.noreply.github.com>
2025-03-31 14:21:09 -0700

    AH-77 Added mongo engine to app and created files for schemas of collections

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,17 @@
 from flask import Flask
-
+from flask_mongoengine import MongoEngine
 from .config import Config
+
+db = MongoEngine()
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
-    from .routes.auth import auth_bp
+    db.init_app(app)
 
-    app.register_blueprint(auth_bp, url_prefix="/api/auth")
+    from .routes.auth import auth_bp
+    app.register_blueprint(auth_bp, url_prefix='/api/auth')
 
     return app
