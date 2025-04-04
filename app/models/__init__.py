36061678f3239bbf2cd01ceba6b39b0ff8819730
Aditya Gambhir <67105262+Aditya-gam@users.noreply.github.com>
2025-04-04 15:56:52 -0700

    Excluded app/models/__init__.py from formatting

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,7 +1,11 @@
 # app/models/__init__.py
+# fmt: off
+# NOTE: This file is excluded from auto-formatting tools (black, isort, ruff)
+# The import order here is deliberate and should not be changed
 from .analytics_data import AnalyticsData
 from .appointment import Appointment
 from .medical_record import MedicalRecord
 from .user import User
 
 __all__ = ["User", "Appointment", "MedicalRecord", "AnalyticsData"]
+# fmt: on
